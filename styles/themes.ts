c62d143155cd70f88e46b6ff46abcd15bd32da44
LFelippDev <lfelippeeng@gmail.com>
2022-04-07 16:49:21 -0300

    Add light theme and themeProvider

---
+++ styles/themes.ts
@@ -0,0 +1,11 @@
+export const light = {
+  colors: {
+    primary_orange: '#FE8235',
+    secundary_orange: '#FEF3E7',
+    principal_text: '#371B34',
+    input_backgroun: '#F8F6F6',
+    input_label: '#828282',
+    list_text: '#573926',
+    delete_product_text: ' #55A06F',
+  },
+};
