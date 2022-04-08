e106196d5330e27ce5d3a05961e2451ee746b0e4
LFelippDev <lfelippeeng@gmail.com>
2022-04-08 03:02:21 -0300

    Refactor styles folder for src folder

--- src/styles/themes.ts
+++ src/styles/themes.ts
@@ -3,9 +3,10 @@ export const light = {
     primary_orange: '#FE8235',
     secundary_orange: '#FEF3E7',
     principal_text: '#371B34',
-    input_backgroun: '#F8F6F6',
+    input_background: '#F8F6F6',
     input_label: '#828282',
     list_text: '#573926',
     delete_product_text: ' #55A06F',
+    white: '#ffffff',
   },
 };
