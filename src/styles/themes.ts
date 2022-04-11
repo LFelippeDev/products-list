c240511ee0133a9c2025cc46415161bf911aa57f
LFelippDev <lfelippeeng@gmail.com>
2022-04-11 09:57:09 -0300

    Add new color for form errors

--- src/styles/themes.ts
+++ src/styles/themes.ts
@@ -5,6 +5,7 @@ export const light = {
     principal_text: '#371B34',
     input_background: '#F8F6F6',
     input_label: '#828282',
+    input_invalid: '#d72404',
     list_text: '#573926',
     delete_product_text: ' #55A06F',
     white: '#ffffff',
